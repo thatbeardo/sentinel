474f082f5ed7be12094a21856e82e1ac76aea136
thatbeardo <hmavani7@gmail.com>
2020-04-09 00:24:51 -0400

    Test coverage 100 percent

--- api/handlers/resources/post_test.go
+++ api/handlers/resources/post_test.go
@@ -9,17 +9,29 @@ import (
 	"github.com/thatbeardo/go-sentinel/testutil"
 )
 
-const payload = `{"data":{"type":"eer","attributes":{"source_id":"anothertest"}}}`
+const noErrors = `{"data":{"type":"resource","attributes":{"source_id":"much-required"}}}`
+const sourceIdAbsent = `{"data":{"type":"resource","attributes":{"someField":"not-much-required"}}}`
 
 func TestPostResourcesOk(t *testing.T) {
 
 	mockService := testutil.NewMockCreateService(createResourceMockResponseNoErrors)
 
 	router := server.SetupRouter(mockService)
-	response, cleanup := testutil.PerformRequest(router, "POST", "/v1/resources/", payload)
+	response, cleanup := testutil.PerformRequest(router, "POST", "/v1/resources/", noErrors)
 	defer cleanup()
 
 	testutil.ValidateResponse(t, response, generateElement(), http.StatusAccepted)
+}
+
+func TestPostResourcesSourceIdAbsent(t *testing.T) {
+
+	mockService := testutil.NewMockCreateService(createResourceMockResponseNoErrors)
+
+	router := server.SetupRouter(mockService)
+	response, cleanup := testutil.PerformRequest(router, "POST", "/v1/resources/", sourceIdAbsent)
+	defer cleanup()
+
+	testutil.ValidateResponse(t, response, "Key: 'Input.Data.Attributes.SourceID' Error:Field validation for 'SourceID' failed on the 'required' tag", http.StatusBadRequest)
 }
 
 func createResourceMockResponseNoErrors(*resource.Input) (resource.Element, error) {
