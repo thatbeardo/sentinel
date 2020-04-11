4a7a9285d7f97f47d28c78edcb959f28469cbd25
thatbeardo <hmavani7@gmail.com>
2020-04-11 13:02:58 -0400

    Model test database error added

--- models/resource/neo4j_test.go
+++ models/resource/neo4j_test.go
@@ -1,20 +1,24 @@
 package resource_test
 
-// import (
-// 	"errors"
-// 	"testing"
+import (
+	"errors"
+	"testing"
 
-// 	"github.com/neo4j/neo4j-go-driver/neo4j"
-// 	"github.com/thatbeardo/go-sentinel/mocks"
-// 	"github.com/thatbeardo/go-sentinel/models/resource"
-// )
+	"github.com/neo4j/neo4j-go-driver/neo4j"
+	"github.com/stretchr/testify/assert"
+	"github.com/thatbeardo/go-sentinel/mocks"
+	"github.com/thatbeardo/go-sentinel/models/resource"
+)
 
-// func TestGetResourcesOk(t testing.T) {
-// 	response := func() (neo4j.Result, error) {
-// 		return nil, errors.New("Database error")
-// 	}
-// 	mockSession := mocks.MockSession{
-// 		SetResponse: response,
-// 	}
-// 	resource.NewNeo4jRepository(mockSession)
-// }
+func TestGetResourcesOk(t *testing.T) {
+	mockSession := mocks.MockSession{
+		SetResponse: errorFromDatabase,
+	}
+	repository := resource.NewNeo4jRepository(mockSession)
+	_, err := repository.Get()
+	assert.NotNil(t, err, "Should not be empty")
+}
+
+func errorFromDatabase() (neo4j.Result, error) {
+	return nil, errors.New("Database error")
+}
