f35e29e3c0fd671c09eacc71361e0aa157fe3d44
thatbeardo <hmavani7@gmail.com>
2020-05-22 23:12:23 -0400

    Update availability zones for subnets

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -4,38 +4,38 @@ module sentinel-vpc {
   vpc_tenancy = "default"
 }
 
-module sentinel-subnet-private-east-1 {
+module sentinel-subnet-private-east-1a {
   source = "../modules/vpc/subnet"
   subnet_cidr_block = "10.0.0.0/28"
-  subnet_name = "private-east-1"
-  availability_zone = "us-east-1"
+  subnet_name = "private-east-1a"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = false
   vpc_id = module.sentinel-vpc.vpc_id
 }
 
-module sentinel-subnet-private-east-2 {
+module sentinel-subnet-private-east-1b {
   source = "../modules/vpc/subnet"
   subnet_cidr_block = "10.0.0.16/28"
-  subnet_name = "private-east-2"
-  availability_zone = "us-east-2"
+  subnet_name = "private-east-1b"
+  availability_zone = "us-east-1b"
   map_public_ip_on_launch = false
   vpc_id = module.sentinel-vpc.vpc_id
 }
 
-module sentinel-subnet-public-east-1 {
+module sentinel-subnet-public-east-1a {
   source = "../modules/vpc/subnet"
   subnet_cidr_block = "10.0.0.32/12"
-  subnet_name = "public-east-1"
-  availability_zone = "us-east-1"
+  subnet_name = "public-east-1a"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
   vpc_id = module.sentinel-vpc.vpc_id
 }
 
-module sentinel-subnet-public-east-2 {
+module sentinel-subnet-public-east-1b {
   source = "../modules/vpc/subnet"
   subnet_cidr_block = "10.0.0.48/12"
-  subnet_name = "public-east-2"
-  availability_zone = "us-east-2"
+  subnet_name = "public-east-1b"
+  availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
   vpc_id = module.sentinel-vpc.vpc_id
 }
@@ -53,20 +53,20 @@ module private_route_table {
 
 module private_route_table_association_subnet_1 {
   source = "../modules/route-table-association"
-  subnet_id = module.sentinel-subnet-private-east-1.subnet_id
+  subnet_id = module.sentinel-subnet-private-east-1a.subnet_id
   route_table_id = module.private_route_table.route_table_id
 }
 
-module private_route_table_association_subnet_2 {
+module private_route_table_association_subnet_1b {
   source = "../modules/route-table-association"
-  subnet_id = module.sentinel-subnet-private-east-2.subnet_id
+  subnet_id = module.sentinel-subnet-private-east-1b.subnet_id
   route_table_id = module.private_route_table.route_table_id
 }
 
 module nat_gateway {
   source = "../modules/nat"
   nat_name = "nat_gateway_east-1"
-  subnet_id = module.sentinel-subnet-public-east-1.subnet_id
+  subnet_id = module.sentinel-subnet-public-east-1a.subnet_id
 }
 
 resource aws_route private_internet_access {
@@ -81,15 +81,15 @@ module public_route_table {
   route_table_name = "public-route-table"
 }
 
-module public_route_table_association_subnet_1 {
+module public_route_table_association_subnet_1a {
   source = "../modules/route-table-association"
-  subnet_id = module.sentinel-subnet-public-east-1.subnet_id
+  subnet_id = module.sentinel-subnet-public-east-1a.subnet_id
   route_table_id = module.public_route_table.route_table_id
 }
 
-module public_route_table_association_subnet_2 {
+module public_route_table_association_subnet_1b {
   source = "../modules/route-table-association"
-  subnet_id = module.sentinel-subnet-public-east-2.subnet_id
+  subnet_id = module.sentinel-subnet-public-east-1b.subnet_id
   route_table_id = module.public_route_table.route_table_id
 }
 
