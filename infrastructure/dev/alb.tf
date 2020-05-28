752053bfbd5459cf1df96f10a63f26eb8d852cc2
thatbeardo <hmavani7@gmail.com>
2020-05-28 18:59:36 -0400

    Deploy target group analysis

--- infrastructure/dev/alb.tf
+++ infrastructure/dev/alb.tf
@@ -6,7 +6,7 @@ resource aws_alb main {
 
 resource aws_alb_target_group app {
   name        = "sentinel-target-group"
-  port        = var.app_port
+  port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
