322589ad3e0b2993a09e3ca2c7f1ebd5d5d7b28b
Anthony Torres <antorres@indigoag.com>
2020-05-29 14:59:13 -0400

    Use bucket arn

--- infrastructure/dev/outputs.tf
+++ infrastructure/dev/outputs.tf
@@ -2,6 +2,6 @@ output alb_hostname {
   value = aws_alb.main.dns_name
 }
 
-output website_bucket {
-  value = aws_s3_bucket.website_bucket.bucket_domain_name
+output website_bucket_arn {
+  value = aws_s3_bucket.website_bucket.arn
 }
