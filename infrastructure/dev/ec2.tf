e67e9b4487a61ee545ff4af15ee8f0670e79031c
Anthony Torres <antorres@indigoag.com>
2020-05-29 13:02:00 -0400

    Deploy react app to s3

--- infrastructure/dev/ec2.tf
+++ infrastructure/dev/ec2.tf
@@ -1,21 +1,5 @@
-data aws_ami neo4j_image {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["neo4j-community-bolt-enabled"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["self"] 
-}
-
 resource aws_instance neo4j {
-  ami             = data.aws_ami.neo4j_image.id
+  ami             = "ami-081a719c938b93687"
   instance_type   = "t2.micro"
   subnet_id       = element(aws_subnet.public.*.id, 0)
   security_groups = [aws_security_group.db.id]
--- infrastructure/dev/outputs.tf
+++ infrastructure/dev/outputs.tf
@@ -1,3 +1,7 @@
 output alb_hostname {
   value = aws_alb.main.dns_name
 }
+
+output website_bucket {
+  value = aws_s3_bucket.website_bucket.bucket_domain_name
+}
