0861f77c189f99e9d5cb41a650154696fd8dffa1
thatbeardo <hmavani7@gmail.com>
2020-05-29 23:32:34 -0400

    aws s3 cp command correction

--- app/src/App.js
+++ app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This version was deployed by the CD workflow! 
         </a>
       </header>
     </div>
